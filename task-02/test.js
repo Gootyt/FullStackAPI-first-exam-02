603d05e4a7fe48084cbcb4dac1c0a0819038c8b0
Gootyt <gootyt@gmail.com>
2021-07-09 15:11:51 +0200

    Second commit done.

--- task-02/test.js
+++ task-02/test.js
@@ -2,12 +2,12 @@
  * 1. Töltsd be a json-állományok kezelését végző modult az azonos mappából, és 
  * emeld ki egy változóba a get metódusát.
  */
-
+const { get } = require('./app');
 /**
  * 2. A teszteléshez vedd ki a kommentblokkból az alábbi kódot.
  */
-/*
+
 ( async () => {
     console.log( await get(8) );
 })();
-*/
+
