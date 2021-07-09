5852065f2093a12473b15b97a4b699d2eb349287
Gootyt <gootyt@gmail.com>
2021-07-09 15:28:41 +0200

    4th commit done.

--- task-04/server.js
+++ task-04/server.js
@@ -3,8 +3,12 @@
  * függvényét.
  */
 
+const http = require('http');
+const { getAll } = require('./functions');
+
 // 2. Definiáld a port értékét 8080 -ra a port változóban.
 
+const port = 8080;
 /**
  * 3. Hozz létre egy HTTP-szervert.
  * A szerver get kérés esetén a getAll függvény segítségével lekéri a listát, 
@@ -12,11 +16,16 @@
  * végül visszaküldi a kliensnek a listában tárolt adatokat.
  */
  const server = http.createServer( async (req, res) => {
-    //
-})
+    const list = await getAll();
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(list, null, 4));
+
+});
+
 /**
  * 4. Állítsd be, hogy a szerver figyelje a port változóban definiált portot.
  */ 
  server.listen( port, () => {
-    //
+    console.log(`Server is running: http://127.0.0.1:${port}`)
  });
